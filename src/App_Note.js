a3b1c706a38a30a87e1a87e03cf4e9fda3cff53f
olukinni_029 <olukinnif`150216029@gmail.com>
2022-08-04 21:35:39 +0100

    update app.js

--- src/App_Note.js
+++ src/App_Note.js
@@ -1,22 +1,34 @@
 require('dotenv').config();
+
+
 const express = require('express');
 const connectDB =require('./Database/db');
 const noteRouter =require('./routes/note.routes');
 
+
 const app = express();
+
 
 connectDB();
 
+
+
 app.use(express.json());
+app.use(express.urlencoded({ extended: true }))
+
 
 const port=process.env.PORT || 3000;
+
+
+
+app.use('/api', noteRouter);
 
 app.get('/',(req,res) => {
     res.send('Digital Note');
 });
 
 
-app.use('/api', noteRouter);
+
 
 app.listen(port,()=>{
     console.log(`Server up and running on port:${port}`);
